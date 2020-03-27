88a371d6a1db4a6be5792f42bc5729a0612bdc5e
Alexander Swed <lexswed@gmail.com>
2020-03-27 19:01:53 +0200

    change Columns space to alignment

--- src/Columns/Columns.tsx
+++ src/Columns/Columns.tsx
@@ -8,16 +8,23 @@ type Props = {
    * Space between columns
    * @default 'm'
    */
-  space: 'xs' | 's' | 'm' | 'l' | 'xl' | 'apart' | 'spread';
+  space: 'xs' | 's' | 'm' | 'l' | 'xl';
+  /**
+   * Alignment of columns
+   * @default 'left'
+   */
+  align?: 'left' | 'center' | 'right' | 'spread' | 'apart';
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-const Columns = React.forwardRef<HTMLDivElement, Props>(({ space = 'm', children, className, ...props }, ref) => {
-  return (
-    <div className={cx('fx-columns', `fx-columns--${space}`, className)} {...props} ref={ref}>
-      {children}
-    </div>
-  );
-});
+const Columns = React.forwardRef<HTMLDivElement, Props>(
+  ({ space = 'm', align = 'left', children, className, ...props }, ref) => {
+    return (
+      <div className={cx('fx-columns', `fx-columns--${space}`, `fx-columns--${align}`, className)} {...props} ref={ref}>
+        {children}
+      </div>
+    );
+  }
+);
 
 if (__DEV__) {
   Columns.displayName = 'FxColumns';

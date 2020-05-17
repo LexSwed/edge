48dae62377a8acd2b1595afed0991ae0094a1116
Alexander Swed <lexswed@gmail.com>
2020-05-17 15:52:29 +0300

    FieldLabel migration

---
+++ src/FieldLabel/FieldLabel.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { fontSize, color, FontSizeProps, ColorProps } from 'styled-system';
+
+export const Label = styled.label<FontSizeProps & ColorProps>`
+  display: inline-block;
+  ${fontSize}
+  ${color}
+  font-weight: 400;
+  line-height: 1;
+  transition: all 0.12s ease-in-out;
+`;

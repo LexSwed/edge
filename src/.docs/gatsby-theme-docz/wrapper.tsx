1b622ef8f569b612d099e8d5562a0f9c67bfeff1
Alexander Swed <lexswed@gmail.com>
2020-05-22 00:18:38 +0300

    cleanup

--- src/.docs/gatsby-theme-docz/wrapper.tsx
+++ src/.docs/gatsby-theme-docz/wrapper.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import { ThemeProvider } from '../../../dist';
-import '../../../dist/styles.css';
 
 import logo from '../../../src/Logo/logo-solid.svg';
 

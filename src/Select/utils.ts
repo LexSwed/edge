9415e5f7935c6013bdeabca960f37e572b43bef1
Alexander Swed <lexswed@gmail.com>
2020-07-08 21:18:27 +0300

    fix(Select): fix type definitions

--- src/Select/utils.ts
+++ src/Select/utils.ts
@@ -5,10 +5,10 @@ export type SelectInputProps<T = string> = {
   message?: string;
   placeholder?: InputProps['placeholder'];
   disabled?: InputProps['disabled'];
-  onSelect: (newValue: T | null) => void;
-  icon: FieldInputProps['icon'];
-  tone: FieldInputProps['tone'];
-  variant: FieldInputProps['variant'];
-  required: FieldInputProps['required'];
-  value: T;
+  onSelect?: (newValue: T | null) => void;
+  icon?: FieldInputProps['icon'];
+  tone?: FieldInputProps['tone'];
+  variant?: FieldInputProps['variant'];
+  required?: FieldInputProps['required'];
+  value?: T;
 };
